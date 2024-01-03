6eb267e8d3c046a6843b3ba0a8a665f86af304dc
aniebietafia <aniebietafia87@gmail.com>
2024-01-03 10:58:11 +0100

    feat: configuring the express app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,47 @@
-import { Request, Response } from "express";
+// import necessary modules
 import * as express from "express";
+import * as morgan from "morgan";
+import * as cors from "cors";
+import helmet from "helmet";
 
-const app = express();
+import { StatusCodes } from "http-status-codes";
 
-app.get("/", (_req: Request, res: Response) => {
-  res.send("Hello World");
+// initialize express app
+const app: express.Application = express();
+
+// import routes
+import routes from "./routes/index.route";
+
+// configure middleware
+app.use(morgan("dev"));
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// mount routes
+app.use("/", routes);
+
+// define index route
+app.get("/", (_req: express.Request, res: express.Response) => {
+  res.status(StatusCodes.OK).json({
+    message: "🚀 Brints Estate Backend Service",
+  });
 });
+
+// define 404 route handler
+app.all("*", (_req: express.Request, res: express.Response) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    message: "🚫 Route not Found",
+  });
+});
+
+// global error handler
+app.use((err: Error, _req: express.Request, res: express.Response) => {
+  console.error(err.stack);
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    message: "🚫 Internal Server Error",
+  });
+});
+
+export default app;

ef598e6e4a50719e2e1f1b858433993f713e6d82
aniebietafia <aniebietafia87@gmail.com>
2024-01-05 15:07:27 +0100

    feat: added generate token to register user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,6 +3,8 @@ import { StatusCodes } from "http-status-codes";
 import tryCatch from "../utils/lib/try-catch.lib";
 import { successResponse, errorResponse } from "../utils/lib/response.lib";
 import { User, IUser } from "../models/user.model";
+import BcryptHelper from "../utils/helpers/bcrypt.helper";
+import { generateVerificationToken } from "../utils/lib/verification-token.lib";
 import {
   SuccessResponseData,
   ErrorResponseData,
@@ -22,9 +24,15 @@ interface RegisterUserRequestBody {
   fullname: string;
   email: string;
   password: string;
+  confirmPassword: string;
   gender: string;
   phone: string;
   role: string;
+  verified: boolean;
+  verificationToken: string;
+  verificationTokenExpire: Date;
+  resetPasswordToken: string;
+  resetPasswordExpire: Date;
 }
 
 interface RegisterUserError {
@@ -41,7 +49,8 @@ type RegisterUserResponse = Response<
 export const registerUser = tryCatch(
   async (req: RegisterUser, res: RegisterUserResponse) => {
     // Get user input
-    const { fullname, email, phone, gender, password, role } = req.body;
+    const { fullname, email, phone, gender, password, confirmPassword } =
+      req.body;
 
     // Validate user input
     if (!fullname || !email) {
@@ -62,14 +71,37 @@ export const registerUser = tryCatch(
       return errorResponse(res, err.message, err.statusCode);
     }
 
+    // Check if password match
+    if (password !== confirmPassword) {
+      const err: RegisterUserError = {
+        message: "Password does not match",
+        statusCode: StatusCodes.BAD_REQUEST,
+      };
+      return errorResponse(res, err.message, err.statusCode);
+    }
+
+    // Hash user password
+    const hashedPassword = await BcryptHelper.hashPassword(password);
+
+    // Make user an admin if it is the first user
+    const users = await User.find();
+    const role = users.length === 0 ? "admin" : "user";
+
+    // Generate verification token
+    const verificationToken = generateVerificationToken()
+
+    // Set verification token expire date to 3 hours
+    
+
     // Create user
     const newUser = await User.create({
       fullname,
       email,
-      password,
+      password: hashedPassword,
       phone,
       role,
       gender,
+      verificationToken
     });
 
     // Return success response

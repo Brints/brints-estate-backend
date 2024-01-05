fafda633e50fe7d10cfc2bb5c7723d69dfc1761e
aniebietafia <aniebietafia87@gmail.com>
2024-01-05 14:28:27 +0100

    feat: set up jwt to generate and verify tokens

---
+++ src/utils/helpers/jwt.helper.ts
@@ -0,0 +1,35 @@
+import * as jwt from "jsonwebtoken";
+import { Secret } from "jsonwebtoken";
+
+/**
+ * @description Generate a JWT token
+ * @param {object} payload
+ * @param expiresIn - time it will take for the token to expire
+ * @returns {string} token
+ */
+
+export const generateToken = (
+  payload: Record<string, unknown>,
+  expiresIn: string | number = "1h"
+): string => {
+  const token = jwt.sign(payload, process.env["JWT_SECRET"] as Secret, {
+    expiresIn,
+  });
+  return token;
+};
+
+/**
+ * @description Verify a JWT token
+ * @param {string} token
+ * @returns {object} payload
+ */
+
+export const verifyToken = (token: string): Record<string, unknown> | null => {
+  try {
+    const payload = jwt.verify(token, process.env["JWT_SECRET"] as Secret);
+    return payload as Record<string, unknown>;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};

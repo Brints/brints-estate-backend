43295668423c310115d279aadf0665f718d0c3a3
aniebietafia <aniebietafia87@gmail.com>
2024-01-05 14:29:06 +0100

    feat: added authentication middleware

---
+++ src/middlewares/authorization/user.authorization.ts
@@ -0,0 +1,48 @@
+import { Request, Response, NextFunction } from "express";
+import { StatusCodes } from "http-status-codes";
+import { errorResponse, ErrorResponseData } from "../../utils/lib/response.lib";
+import { verifyToken } from "../../utils/helpers/jwt.helper";
+import { User, IUser } from "../../models/user.model";
+
+interface AuthenticatedUserRequest extends Request {
+  user?: Record<string, unknown>;
+}
+
+type AuthenticatedUserResponse = Response<ErrorResponseData>;
+
+export const authenticatedUser = async (
+  req: AuthenticatedUserRequest,
+  res: AuthenticatedUserResponse,
+  next: NextFunction
+) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return errorResponse(res, "Unauthorized", StatusCodes.UNAUTHORIZED);
+    }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      return errorResponse(res, "Unauthorized", StatusCodes.UNAUTHORIZED);
+    }
+
+    const payload = verifyToken(token);
+    if (!payload) {
+      return errorResponse(
+        res,
+        "Invalid Token. Please login again",
+        StatusCodes.FORBIDDEN
+      );
+    }
+
+    const user = await User.findOne({ token });
+
+    // get user from database and attach to request object
+    req.user = user as IUser;
+    next();
+    return;
+  } catch (error) {
+    console.error(error);
+    return errorResponse(res, "Unauthorized", StatusCodes.UNAUTHORIZED);
+  }
+};

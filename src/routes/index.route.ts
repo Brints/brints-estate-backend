7fd84e333fe3f151c3e8d98ad40ec2c77dbaf6e8
aniebietafia <aniebietafia87@gmail.com>
2024-01-05 20:02:45 +0100

    feat: export all routes

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 
 // import routes
+import userRouter from "./user.route";
 
 const router: Router = Router();
+
+// routes
+router.use("/user", userRouter);
 
 export default router;

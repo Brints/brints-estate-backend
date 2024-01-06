d52ee7d65e8025b7c3a29c9f170cdb03c9a81b59
aniebietafia <aniebietafia87@gmail.com>
2024-01-06 15:20:45 +0100

    feat: added validation middleware to register user endpoint

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -6,11 +6,15 @@ import * as userController from "../controllers/user.controller";
 // middlewares
 // import * as userAuthorization from "../middlewares/authorization/user.authorization";
 
+// validators
+import { validateUserRegistration } from "../middlewares/validations/user.validation";
+
 const userRouter: Router = Router();
 
 // routes
 userRouter.post(
   "/register",
+  validateUserRegistration,
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   userController.registerUser
 );

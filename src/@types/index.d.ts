2d433240f0ce5daaa763a8a9ff2b8378678ec814
aniebietafia <aniebietafia87@gmail.com>
2024-01-06 08:23:43 +0100

    feat: type declaration file

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,5 +1,51 @@
-export interface SuccessResponseData<T> {
+import { Document } from "mongoose";
+// import { Request } from "express";
+declare interface IUser extends Document {
+  fullname: string;
+  email: string;
+  password: string;
+  gender: string;
+  phone: string;
+  role: string;
+  verified: boolean;
+  verificationToken: string;
+  verificationTokenExpire: Date;
+  resetPasswordToken?: string;
+  resetPasswordExpire?: Date;
+  [key: string]: unknown;
+}
+
+declare interface RegisterUserRequestBody {
+  fullname: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  gender: string;
+  phone: string;
+  role: string;
+  verified: boolean;
+  verificationToken: string;
+  verificationTokenExpire: Date;
+  resetPasswordToken?: string;
+  resetPasswordExpire?: Date;
+}
+
+declare interface RegisterUserError {
+  message: string;
+  statusCode: number;
+}
+
+declare interface SuccessResponseData<T> {
   message: string;
   data: T;
   statusCode: number;
+}
+
+declare interface ErrorResponseData {
+  success: boolean;
+  error: {
+    type: string;
+    statusCode: number;
+    message: string;
+  };
 }

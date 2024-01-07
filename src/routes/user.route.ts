2b4472c3658275acb0b2cbdc7b63944b5467bff5
aniebietafia <aniebietafia87@gmail.com>
2024-01-07 16:40:17 +0100

    feat: added upload middleware to register endpoint

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { multerConfig } from "../config/multer.config";
+
+const upload = multerConfig.array("image", 5);
 
 // controllers
 import * as userController from "../controllers/user.controller";
@@ -14,6 +17,7 @@ const userRouter: Router = Router();
 // routes
 userRouter.post(
   "/register",
+  upload,
   validateUserRegistration,
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   userController.registerUser

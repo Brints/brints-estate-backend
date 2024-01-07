35ca7091a065233e601fdfe41200fabd43376cc0
aniebietafia <aniebietafia87@gmail.com>
2024-01-07 16:38:30 +0100

    feat: setup multer configurations

---
+++ src/config/multer.config.ts
@@ -0,0 +1,44 @@
+import * as multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinaryConfig from "./cloudinary.config";
+import { CustomParams } from "../@types";
+
+/*
+process.env["CLOUDINARY_CLOUD_NAME"] as string,
+  process.env["CLOUDINARY_API_KEY"] as string,
+  process.env["CLOUDINARY_API_SECRET"] as string
+  */
+
+const cloudinaryConfigOptions = cloudinaryConfig(
+  process.env["CLOUDINARY_CLOUD_NAME"] as string,
+  process.env["CLOUDINARY_API_KEY"] as string,
+  process.env["CLOUDINARY_API_SECRET"] as string
+);
+
+cloudinary.config(cloudinaryConfigOptions);
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "brints-estate",
+    allowedFormats: ["jpg", "png", "jpeg"],
+  } as CustomParams,
+});
+
+const MAX_FILE_SIZE = 1024 * 1024 * 5; // 5MB
+
+export const multerConfig = multer({
+  storage,
+  limits: {
+    fileSize: MAX_FILE_SIZE,
+  },
+  fileFilter: (_req, file, callback) => {
+    if (!file.mimetype.startsWith("image/")) {
+      return callback(null, false);
+    }
+    callback(null, true);
+  },
+});
+
+export { cloudinary };

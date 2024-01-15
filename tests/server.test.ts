d8cd9618ae47d5fadd9189ecee2fb05b71fd8899
aniebietafia <aniebietafia87@gmail.com>
2024-01-15 14:40:17 +0100

    test case for express app

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -3,6 +3,7 @@ import app from "../src/app";
 import { StatusCodes } from "http-status-codes";
 import { describe, it, expect } from "@jest/globals";
 
+// This is a test for the server, not the database
 describe("Server Test", () => {
   it("should respond with 200 status for GET /", async () => {
     const testRequest = requestAgent(app) as unknown as SuperTest<Test>;

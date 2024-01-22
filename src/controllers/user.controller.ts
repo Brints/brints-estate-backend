d54dda6df74d514899c3345f4ecaeb78e271ac1b
aniebietafia <aniebietafia87@gmail.com>
2024-01-22 16:11:53 +0100

    fix: updating avatar and storing to database

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -969,6 +969,8 @@ export const updateUserProfile = tryCatch(
 
     const { avatar, fullname, gender, phone } = req.body;
 
+    CapitalizeFirstLetter.capitalizeFirstLetter(fullname);
+
     // Get user id from request object
     const userId = (req as unknown as UserObject).user;
 

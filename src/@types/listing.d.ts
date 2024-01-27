48a63bf1e30060f14c9e2fbb0c3bd3b06e105d92
aniebietafia <aniebietafia87@gmail.com>
2024-01-27 13:10:44 +0100

    declaring types for the listings

--- src/@types/listing.d.ts
+++ src/@types/listing.d.ts
@@ -22,3 +22,46 @@ declare interface IListing extends Document {
   images: { url: string; filename: string }[];
   [key: string]: unknown;
 }
+
+declare interface RegisterListingRequestBody {
+  title: string;
+  description: string;
+  price: number;
+  discount?: number;
+  location: string;
+  address: string;
+  city: string;
+  state: string;
+  country: string;
+  zipcode?: string;
+  status: string;
+  type: string;
+  bedroom: number;
+  bathroom: number;
+  amenities: string[];
+  images: { url: string; filename: string }[];
+}
+
+declare interface SuccessResponseData<T> {
+  message: string;
+  data: T;
+  statusCode: number;
+}
+
+declare interface ErrorResponseData {
+  message: string;
+  statusCode: number;
+}
+
+declare type ListingResponse = Response<
+  SuccessResponseData<IListing> | ErrorResponseData
+>;
+
+declare interface ListingError {
+  message: string;
+  statusCode: number;
+}
+
+declare interface ListingObject extends Request {
+  listing: IListing;
+}

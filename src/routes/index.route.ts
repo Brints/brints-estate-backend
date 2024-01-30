2f09993c4e9d29b3988b431873c2646fdbd7150c
aniebietafia <aniebietafia87@gmail.com>
2024-01-30 10:40:18 +0100

    feat: added listing router

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 // import routes
 import userRouter from "./user.route";
+import listingRouter from "./listing.route";
 
 const router: Router = Router();
 
 // routes
 router.use("/user", userRouter);
+router.use("/listing", listingRouter);
 
 export default router;

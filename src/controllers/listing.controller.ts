6a88714348d56a84a079cb2fffd40d57d3a5a8e2
aniebietafia <aniebietafia87@gmail.com>
2024-02-05 22:42:05 +0100

    feat: update listings

--- src/controllers/listing.controller.ts
+++ src/controllers/listing.controller.ts
@@ -314,3 +314,147 @@ export const searchListings = tryCatch(
     );
   }
 );
+
+/**
+ * @description Update Listing
+ * @route PUT /listing/:listingId
+ * @access Private
+ * @param {Request<ParamsDictionary, unknown, RegisterListingRequestBody>} req
+ * @param {ListingResponse} res
+ * @returns {Promise<ListingResponse | void>}
+ * @throws {Error}
+ */
+
+export const updateListing = tryCatch(
+  async (req: CreateListingRequest, res: ListingResponse): Promise<unknown> => {
+    // destructure request body
+    const {
+      title,
+      description,
+      price,
+      discount,
+      address,
+      city,
+      state,
+      country,
+      status,
+      type,
+      bedroom,
+      bathroom,
+      amenities,
+      images,
+    } = req.body;
+
+    // fetch the user from request object
+    const userId = (req as unknown as UserObject).user;
+
+    // set status to rent if not provided and type to apartment if not provided
+    // if (!status) {
+    //   status = "rent";
+    // }
+
+    // validate request body
+    if (
+      !title ||
+      !description ||
+      !price ||
+      !address ||
+      !city ||
+      !state ||
+      !country ||
+      !status ||
+      !type ||
+      !bedroom ||
+      !bathroom ||
+      !amenities ||
+      !images
+    ) {
+      const error: ListingError = {
+        message: "Please fill in all fields",
+        statusCode: StatusCodes.BAD_REQUEST,
+      };
+      return errorResponse(res, error.message, error.statusCode);
+    }
+
+    // check if the user is a user
+    if (userId.role === "user") {
+      const error: ListingError = {
+        message:
+          "You are cannot create a listing. Upgrade to an agent/landlord account",
+        statusCode: StatusCodes.UNAUTHORIZED,
+      };
+      return errorResponse(res, error.message, error.statusCode);
+    }
+
+    // convert the amenities string to array
+    const amenitiesArray = amenities.split(",");
+    const formattedAmenities = amenitiesArray.map((amenity) => amenity.trim());
+
+    // capitalize the first letter of the state and city
+    const formattedState = CapitalizeFirstLetter.capitalizeFirstLetter(state);
+    const formattedCity = CapitalizeFirstLetter.capitalizeFirstLetter(city);
+    const formattedCountry =
+      CapitalizeFirstLetter.capitalizeFirstLetter(country);
+    const formattedTitle = CapitalizeFirstLetter.capitalizeFirstLetter(title);
+
+    // check if the description is more than 160 characters
+    const descriptionLength =
+      ListingHelper.descriptionNotMoreThan160Characters(description);
+    if (descriptionLength > 160) {
+      const error: ListingError = {
+        message: "Description cannot be more than 160 characters",
+        statusCode: StatusCodes.BAD_REQUEST,
+      };
+      return errorResponse(res, error.message, error.statusCode);
+    }
+
+    // create new listing
+    const listing = await Listing.create({
+      title: formattedTitle,
+      description,
+      price,
+      discount,
+      address,
+      city: formattedCity,
+      state: formattedState,
+      country: formattedCountry,
+      status,
+      type,
+      bedroom,
+      bathroom,
+      amenities: formattedAmenities,
+      images,
+      owner: userId._id,
+    });
+
+    CapitalizeFirstLetter.capitalizeFirstLetter(type);
+
+    // upload images to cloudinary
+    if (req.files) {
+      //   const { images } = req.files as { images: Express.Multer.File[] };
+      const images = req.files as Express.Multer.File[];
+      const uploadImages = images.map(async (image) => {
+        const result = await cloudinary.uploader.upload(image.path);
+        return { url: result.secure_url, filename: result.public_id };
+      });
+      listing.images = await Promise.all(uploadImages);
+    }
+
+    // save listing
+    await listing.save();
+
+    // return success response
+    const data: IListing = listing;
+    const success: SuccessResponseData<IListing> = {
+      message: "Listing created successfully",
+      data,
+      statusCode: StatusCodes.CREATED,
+    };
+    return successResponse(
+      res,
+      success.message,
+      success.data,
+      success.statusCode
+    );
+  }
+);

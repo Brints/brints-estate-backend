c11fea92f79b75dbb519aabf6b3eced37a15772b
aniebietafia <aniebietafia87@gmail.com>
2024-02-07 15:34:11 +0100

    defining definitions types for favorites

---
+++ src/@types/favorite.d.ts
@@ -0,0 +1,44 @@
+import { Document } from "mongoose";
+import { Response } from "express";
+
+declare type ResponseData<T> = Response<T>;
+
+declare interface IFavorite extends Document {
+  listing: string;
+  user: string;
+  [key: string]: unknown;
+}
+
+declare interface FavoriteRequestBody {
+  listing: string;
+  user: string;
+  [key: string]: unknown;
+}
+
+declare interface SuccessResponseData<T> {
+  message: string;
+  data: T;
+  statusCode: number;
+}
+
+declare interface ErrorResponseData {
+  success: boolean;
+  error: {
+    type: string;
+    message: string;
+    statusCode: number;
+  };
+}
+
+declare type FavoriteResponse = Response<
+  SuccessResponseData<IFavorite> | ErrorResponseData
+>;
+
+declare interface FavoriteError {
+  message: string;
+  statusCode: number;
+}
+
+declare interface FavoriteObject extends Request {
+  favorite: IFavorite;
+}

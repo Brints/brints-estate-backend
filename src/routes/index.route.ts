0784a2a84804759a082ba70b851f17270bf2301e
aniebietafia <aniebietafia87@gmail.com>
2024-02-08 11:14:12 +0100

    feat: added favourites router

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 // import routes
 import userRouter from "./user.route";
 import listingRouter from "./listing.route";
+import favoriteRouter from "./favorites.route";
 
 const router: Router = Router();
 
 // routes
 router.use("/user", userRouter);
 router.use("/listing", listingRouter);
+router.use("/listing", favoriteRouter);
 
 export default router;

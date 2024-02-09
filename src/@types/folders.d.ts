8b8b71b1df55cd991f01536dd29583f225b0af12
aniebietafia <aniebietafia87@gmail.com>
2024-02-09 15:01:17 +0100

    feat: added folder response type

---
+++ src/@types/folders.d.ts
@@ -0,0 +1,51 @@
+import { Document } from "mongoose";
+import { Response } from "express";
+
+declare type ResponseData<T> = Response<T>;
+
+declare interface IFolder extends Document {
+  name: string;
+  user: string;
+  [key: string]: unknown;
+}
+
+declare interface SuccessResponseData<T> {
+  message: string;
+  data: T;
+  statusCode: number;
+}
+
+declare interface ErrorResponseData {
+  success: boolean;
+  error: {
+    type: string;
+    message: string;
+    statusCode: number;
+  };
+}
+
+declare type FolderResponse = Response<
+  SuccessResponseData<IFolder> | ErrorResponseData
+>;
+
+declare interface FolderError {
+  message: string;
+  statusCode: number;
+}
+
+declare interface FolderRequestBody {
+  name: string;
+  user: string;
+  [key: string]: unknown;
+}
+
+declare interface FolderUpdateRequestBody {
+  name: string;
+  [key: string]: unknown;
+}
+
+declare type FolderResponseData = ResponseData<FolderResponse>;
+
+declare type FolderErrorData = ResponseData<FolderError>;
+
+// declare type FolderRequest = Request<unknown, unknown, FolderRequestBody, unknown>;

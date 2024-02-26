ee7ce8b9b5a5fd0233f2b1dc197187368db2c294
aniebietafia <aniebietafia87@gmail.com>
2024-02-26 15:43:22 +0100

    converted to executable file

--- github.sh
+++ github.sh
@@ -3,4 +3,7 @@
 # This script is used to pull the changes to the github repository
 
 # Change the directory to the repository
+cd /var/www/brints-estate-backend
+
+# Pull the changes from the repository
 git pull origin main

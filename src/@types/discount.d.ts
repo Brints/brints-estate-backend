9d1380c48873a1ecf8ffb601714303e27fa60097
aniebietafia <aniebietafia87@gmail.com>
2024-03-09 14:32:44 +0100

    discount declaration types

---
+++ src/@types/discount.d.ts
@@ -0,0 +1,50 @@
+import { Document } from "mongoose";
+import { Response } from "express";
+
+declare type ResponseData<T> = Response<T>;
+
+declare interface IDiscount extends Document {
+  code: string;
+  discount: number;
+  expireAt: Date;
+}
+
+declare interface DiscountResponse {
+  message: string;
+  data: IDiscount;
+  statusCode: number;
+}
+
+declare interface DiscountError {
+  message: string;
+  statusCode: number;
+}
+
+declare interface DiscountObject extends Request {
+  discount: IDiscount;
+}
+
+declare interface SuccessResponseData<T> {
+  message: string;
+  data: T;
+  statusCode: number;
+}
+
+declare interface ErrorResponseData {
+  success: boolean;
+  error: {
+    type: string;
+    message: string;
+    statusCode: number;
+  };
+}
+
+export {
+  ResponseData,
+  IDiscount,
+  DiscountResponse,
+  DiscountError,
+  DiscountObject,
+  SuccessResponseData,
+  ErrorResponseData,
+};

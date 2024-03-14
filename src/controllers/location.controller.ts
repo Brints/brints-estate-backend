c0895f32ecadbc2b8e9bf7ca7c7290a053c08625
aniebietafia <aniebietafia87@gmail.com>
2024-03-14 08:39:41 +0100

    feat: location controller

--- src/controllers/location.controller.ts
+++ src/controllers/location.controller.ts
@@ -1,10 +1,22 @@
-// import libraries
-import { StatusCodes } from "http-status-codes";
+// // import libraries
+// import { StatusCodes } from "http-status-codes";
 
-// import custom libraries
-import tryCatch from "../utils/lib/try-catch.lib";
-import { Location } from "../models/location.model";
-import { successResponse, errorResponse } from "../utils/lib/response.lib";
+// // import custom libraries
+// import tryCatch from "../utils/lib/try-catch.lib";
+// import { Location } from "../models/location.model";
+// import { successResponse, errorResponse } from "../utils/lib/response.lib";
 
-// import types
-import { LocationResponse, LocationError, ILocation } from "../@types/location";
+// // import types
+// import { LocationResponse, LocationError, ILocation } from "../@types/location";
+
+// /**
+//  * @desc    Create location
+//  * @route   POST /location/add-location
+//  * @param  {Request} req
+//  * @param  {Response} res
+//  * @access  Private
+//  */
+
+// type LocationObject = Request<unknown, unknown, ILocation, unknown>;
+
+// export const createLocation =

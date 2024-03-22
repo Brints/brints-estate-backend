115423b921be773c3f40dd9514bef78d8f657c1d
aniebietafia <aniebietafia87@gmail.com>
2024-03-22 18:49:03 +0100

    feat: creating reusable interfaces

---
+++ src/@types/base.d.ts
@@ -0,0 +1,23 @@
+import { Response } from "express";
+
+declare type ResponseData<T> = Response<T>;
+
+export interface ResponseError {
+  message: string;
+  statusCode: number;
+}
+
+export interface SuccessResponseData<T> {
+  message: string;
+  data: T;
+  statusCode: number;
+}
+
+export interface ErrorResponseData {
+  success: boolean;
+  error: {
+    type: string;
+    message: string;
+    statusCode: number;
+  };
+}

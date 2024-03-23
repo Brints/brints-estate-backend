61c3f77bf15851bc0c2f6b951c15886f02b02ef2
aniebietafia <aniebietafia87@gmail.com>
2024-03-23 16:41:03 +0100

    feat: added admin route

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 // import routes
+import adminRoute from "./admin.route";
 import userRouter from "./user.route";
 import listingRouter from "./listing.route";
 import favoriteRouter from "./favorites.route";
@@ -8,6 +9,7 @@ import favoriteRouter from "./favorites.route";
 const router: Router = Router();
 
 // routes
+router.use("/admin", adminRoute);
 router.use("/user", userRouter);
 router.use("/listing", listingRouter);
 router.use("/listings", favoriteRouter);

01d48bce9a66f6784fc2e457cfbb9743593f4efb
aniebietafia <aniebietafia87@gmail.com>
2024-03-23 16:38:43 +0100

    feat: added endpoint for getting about

---
+++ src/routes/admin.route.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Router } from "express";
+import { multerConfig } from "../config/multer.config";
+
+const upload = multerConfig.array("image", 2);
+
+// import * as adminController from "../controllers/admin.controller";
+
+import { AboutController } from "../controllers/admin.controller";
+
+import * as authMiddleware from "../middlewares/authorization/user.authorization";
+
+// validators here
+
+const adminRouter: Router = Router();
+
+// routes
+class AdminRouter {
+  static routes(): Router {
+    adminRouter.post(
+      "/about/add",
+      authMiddleware.authenticatedUser,
+      upload,
+      AboutController.createAbout
+    );
+    adminRouter.get("/about-us/:aboutId", AboutController.getAbout);
+
+    return adminRouter;
+  }
+}
+
+export default AdminRouter.routes();
+
+// adminRouter.post(
+//   "/about/add",
+//   authMiddleware.authenticatedUser,
+//   upload,
+//   AboutController.createAbout
+// );
+// adminRouter.get("/about-us/:aboutId", AboutController.getAbout);
+
+// export default adminRouter;

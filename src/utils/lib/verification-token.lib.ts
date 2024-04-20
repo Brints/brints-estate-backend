d479b103785290822e83df63c1901a1cb719c3a8
aniebietafia <aniebietafia87@gmail.com>
2024-04-20 12:33:14 +0100

    feat: added function to generate 6 digit otp

--- src/utils/lib/verification-token.lib.ts
+++ src/utils/lib/verification-token.lib.ts
@@ -3,3 +3,13 @@ import * as crypto from "crypto";
 export const generateVerificationToken = () => {
   return crypto.randomBytes(28).toString("hex");
 };
+
+// export const generateOTP = () => {
+//   return Math.floor(100000 + Math.random() * 900000).toString();
+// };
+
+export const generateOTP = (len: number) => {
+  return Math.floor(100000 + Math.random() * 900000)
+    .toString()
+    .slice(0, len);
+};

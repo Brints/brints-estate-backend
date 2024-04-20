7f16dea366baa0c91a9f55eea42d0dbd6a64aa92
aniebietafia <aniebietafia87@gmail.com>
2024-04-20 12:19:33 +0100

    feat: user auth model

--- src/models/userAuth.model.ts
+++ src/models/userAuth.model.ts
@@ -21,4 +21,4 @@ const userAuth: Schema = new Schema(
 );
 
 // UserAuth model
-export const UserAuthModel = model<UserAuth>("UserAuth", userAuth);
+export const UserAuth = model<UserAuth>("UserAuth", userAuth);

50f165e1af03fe7babc4531025542aa62dd271ec
aniebietafia <aniebietafia87@gmail.com>
2024-04-22 07:30:11 +0100

    feat: added user authentication model

--- src/models/userAuth.model.ts
+++ src/models/userAuth.model.ts
@@ -5,10 +5,11 @@ import { UserAuth } from "../@types";
 const userAuth: Schema = new Schema(
   {
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    otp: { type: String, required: true },
-    verificationToken: { type: String, required: true },
+    otp: { type: String },
+    otpExpiration: { type: Date },
+    verificationToken: { type: String },
     resetPasswordToken: { type: String },
-    tokenExpiration: { type: Date, required: true },
+    tokenExpiration: { type: Date },
     emailVerified: { type: Boolean, default: false },
     phoneNumberVerified: { type: Boolean, default: false },
     status: {

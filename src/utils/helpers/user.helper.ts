09468d4ca5328836839dbc3ea0e148228abd0de6
aniebietafia <aniebietafia87@gmail.com>
2024-04-26 11:39:53 +0100

    feat: function to calculate discount

--- src/utils/helpers/user.helper.ts
+++ src/utils/helpers/user.helper.ts
@@ -35,4 +35,31 @@ export class ListingHelper {
 
     return description.length;
   }
+
+  static calculateDiscountedPrice(price: number, discount: number): number {
+    return price - (price * discount) / 100;
+  }
+
+  static async getCoordinatesFromAddress(address: string): Promise<string> {
+    const encodedAddress = encodeURI(address);
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${process.env["GOOGLE_MAP_API_KEY"]}`;
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Unable to fetch coordinates from address");
+    }
+
+    const data = (await response.json()) as {
+      results: { geometry: { location: { lat: number; lng: number } } }[];
+    };
+
+    if (data.results.length === 0) {
+      throw new Error("No coordinates found for the address");
+    }
+
+    const coordinates = `${data?.results[0]?.geometry?.location?.lat}, ${data?.results[0]?.geometry?.location?.lng}`;
+
+    return coordinates;
+  }
 }

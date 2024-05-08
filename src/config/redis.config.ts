29a09ece00af2404f3f6bc982c9ee498e457524e
aniebietafia <aniebietafia87@gmail.com>
2024-05-08 13:24:24 +0100

    redis config

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,8 +1,8 @@
-import * as redis from "redis";
+// import * as redis from "redis";
 
-const redisClient = redis.createClient({
-  host: process.env["REDIS_HOST"] || "localhost",
-  port: process.env["REDIS_PORT"] || 6379,
-});
+// const redisClient = redis.createClient({
+//   host: process.env["REDIS_HOST"] || "localhost",
+//   port: process.env["REDIS_PORT"] || 6379,
+// });
 
-export default redisClient;
+// export default redisClient;
